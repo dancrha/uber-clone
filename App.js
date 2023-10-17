9aab5d14eef6e1f16aa890b9c450fff3d4ad9568
Dan Crha <44410856+danielcrha@users.noreply.github.com>
2023-10-17 14:55:51 -0400

    redux set up

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Let's build uber!</Text>
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text>Let's build uber</Text>
+      </View>
+    </Provider>
   );
 }
 
