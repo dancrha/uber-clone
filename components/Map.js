7c6f1db076239778f29171abcafbc65e49d9f92a
Dan Crha <44410856+danielcrha@users.noreply.github.com>
2023-10-19 18:55:11 -0400

    directions good

--- components/Map.js
+++ components/Map.js
@@ -6,46 +6,6 @@ import { useSelector } from "react-redux";
 import { selectDestination, selectOrigin } from "../slices/navSlice";
 import MapViewDirections from "react-native-maps-directions";
 import { GOOGLE_MAPS_APIKEY } from "@env";
-import Geocoding from "react-native-geocoding";
-
-Geocoding.init(process.env.GOOGLE_MAPS_APIKEY);
-
-const getCoordinatesFromAddress = async (address) => {
-  try {
-    const response = await Geocoding.from(address);
-    const { results } = response;
-    if (results.length > 0) {
-      const { geometry } = results[0];
-      const { location } = geometry;
-      return {
-        latitude: location.lat,
-        longitude: location.lng,
-      };
-    } else {
-      throw new Error("No results found");
-    }
-  } catch (error) {
-    console.error("Error getting coordinates from address:", error);
-    return null;
-  }
-};
-
-async function yourAsyncFunction(data, details = null) {
-  console.log(data, details);
-  const coordinates = await getCoordinatesFromAddress(data.description);
-  console.log(coordinates);
-  if (coordinates) {
-    dispatch(
-      setDestination({
-        location: coordinates,
-        description: data.description,
-      })
-    );
-    navigation.navigate("RideOptionsCard");
-  } else {
-    console.log("No coordinates found.");
-  }
-}
 
 const Map = () => {
   const origin = useSelector(selectOrigin);
